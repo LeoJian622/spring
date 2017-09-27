34a8cd23a57e4c20a90ecdb60c14de890a26cfd6
LeoJian622 <hualuoyu_yks@qq.com>
2017-09-27 14:56:33 +0800

    eg 08 06spring4_autowiring time:2017-9-27 14:56:30

---
+++ 06spring4_autowiring/src/cn/sxt/service/impl/UserServiceImpl.java
@@ -0,0 +1,19 @@
+package cn.sxt.service.impl;
+
+import cn.sxt.dao.UserDao;
+import cn.sxt.service.UserService;
+
+public class UserServiceImpl implements UserService {
+	private UserDao userDao = null;
+
+	public UserServiceImpl(UserDao userDao) {
+		super();
+		this.userDao = userDao;
+	}
+
+	@Override
+	public void getUser() {
+		userDao.getUser();
+	}
+
+}
