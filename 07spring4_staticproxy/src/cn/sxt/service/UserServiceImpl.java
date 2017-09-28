15efd3b13dbb56a357d560506b3ce9ada4f48399
LeoJian622 <hualuoyu_yks@qq.com>
2017-09-28 15:12:42 +0800

    spring+mybatis+mysql 17 12spring4_mybatis time:2017-9-28 15:12:39

--- 07spring4_staticproxy/src/cn/sxt/service/UserServiceImpl.java
+++ 07spring4_staticproxy/src/cn/sxt/service/UserServiceImpl.java
@@ -5,7 +5,7 @@ public class UserServiceImpl implements UserService {
 	@Override
 	public void add() {
 		//公共的业务--日志，安全，权限，缓存，事物等等
-		//a.log();增加依赖等。
+		//a.log();增加依赖等。--分离的思想;osgi--java模块开发
 		System.out.println("add");
 	}
 
---
+++ 12spring4_mybatis/src/cn/sxt/dao/impl/UserDaoImpl.java
@@ -0,0 +1,23 @@
+package cn.sxt.dao.impl;
+
+import java.util.List;
+
+import org.mybatis.spring.SqlSessionTemplate;
+
+import cn.sxt.dao.UserDao;
+import cn.sxt.vo.User;
+
+public class UserDaoImpl implements UserDao {
+	private SqlSessionTemplate sqlSessionTemplate;
+
+	public void setSqlSessionTemplate(SqlSessionTemplate sqlSessionTemplate) {
+		this.sqlSessionTemplate = sqlSessionTemplate;
+	}
+
+	@Override
+	public List<User> selectUsers() {
+		return sqlSessionTemplate
+				.selectList("cn.sxt.vo.user.mapper.selectAll");
+	}
+
+}
